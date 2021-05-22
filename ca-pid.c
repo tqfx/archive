b004586857530b04090a1ce5dab44eaae7fb6507
tqfx <tqfx@foxmail.com>
2021-05-22 21:22:15 +0800

    Edit comment on ca-pid

---
+++ ca-pid.c
@@ -0,0 +1,301 @@
+/*!
+ @file           ca-pid.c
+ @brief          Proportional Integral Derivative Algorithm
+ @details        A proportional–integral–derivative controller
+                 (PID controller or three-term controller) is a control loop
+                 mechanism employing feedback that is widely used in industrial
+                 control systems and a variety of other applications requiring
+                 continuously modulated control.
+                 - Position pid control
+ \f{aligned}
+    u(k) &= K_p e(k) + K_i \sum^k_{i=0} q e(i) + K_d [e(k) - e(k-1)]\\
+         &= (K_p + K_d) e(k) + (-K_d)e(k - 1) + K_i \sum^k_{i=0} q e(i)\\
+         &\begin{cases}q = 0 & |K_i \sum\limits^{k-1}_{i=0} e(i)| > E, e(k)e(k - 1) < 0 \\
+          q = 1\end{cases}
+ \f}
+                 - Incremental pid control
+ \f{aligned}
+    \Delta u(k) &= K_p [e(k) − e(k - 1)]
+                 + K_i e(k)
+                 + K_d [e(k) + e(k - 2) − 2 e(k - 1)] \\
+                &= (K_p + K_i + K_d) e(k)
+                 + (-K_p - 2 K_i) e(k - 1)
+                 + K_d e(k - 2)
+ \f}
+                 https://en.wikipedia.org/wiki/PID_controller
+ @author         tqfx tqfx@foxmail.com
+ @version        0
+ @date           2021-05-22
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+#include "ca-pid.h"
+
+#include <stdarg.h>
+
+/*!
+ @cond
+*/
+
+#undef LIMIT
+#define LIMIT(x, min, max) ((x) > (max)        \
+                                ? (max)        \
+                                : ((x) < (min) \
+                                       ? (min) \
+                                       : (x)))
+
+/*!
+ @endcond
+*/
+
+void ca_pid_f32_init(ca_pid_f32_t *pid,
+                     ca_pid_mode_t mode,
+                     const float   k[3],
+                     const float   omin,
+                     const float   omax,
+                     ...)
+{
+    va_list ap;
+
+    /* Set mode of PID Control */
+    pid->mode = mode;
+    /* Set minimum output */
+    pid->omin = omin;
+    /* Set maximum output */
+    pid->omax = omax;
+
+    switch (pid->mode)
+    {
+    case CA_PID_POSITION:
+    {
+        /* Set maximum intergral output */
+        va_start(ap, omax);
+        pid->omaxi = (float)va_arg(ap, double);
+        if (pid->omaxi < 0)
+        {
+            pid->omaxi = -pid->omaxi;
+        }
+        va_end(ap);
+
+        /* Set derived gain */
+        pid->a[0] = k[0] + k[2];
+        pid->a[1] = -k[2];
+        pid->a[2] = k[1];
+
+        break;
+    }
+    case CA_PID_DELTA:
+    {
+        /* Reset maximum intergral output */
+        pid->omaxi = 0;
+
+        /* Set derived gain */
+        pid->a[0] = k[0] + k[1] + k[2];
+        pid->a[1] = -k[0] - 2 * k[2];
+        pid->a[2] = k[2];
+
+        break;
+    }
+
+    default:
+        break;
+    }
+}
+
+void ca_pid_f64_init(ca_pid_f64_t *pid,
+                     ca_pid_mode_t mode,
+                     const double  k[3],
+                     const double  omin,
+                     const double  omax,
+                     ...)
+{
+    va_list ap;
+
+    /* Set mode of PID Control */
+    pid->mode = mode;
+    /* Set minimum output */
+    pid->omin = omin;
+    /* Set maximum output */
+    pid->omax = omax;
+
+    switch (pid->mode)
+    {
+    case CA_PID_POSITION:
+    {
+        /* Set maximum intergral output */
+        va_start(ap, omax);
+        pid->omaxi = va_arg(ap, double);
+        if (pid->omaxi < 0)
+        {
+            pid->omaxi = -pid->omaxi;
+        }
+        va_end(ap);
+
+        /* Set derived gain */
+        pid->a[0] = k[0] + k[2];
+        pid->a[1] = -k[2];
+        pid->a[2] = k[1];
+
+        break;
+    }
+    case CA_PID_DELTA:
+    {
+        /* Reset maximum intergral output */
+        pid->omaxi = 0;
+
+        /* Set derived gain */
+        pid->a[0] = k[0] + k[1] + k[2];
+        pid->a[1] = -k[0] - 2 * k[2];
+        pid->a[2] = k[2];
+
+        break;
+    }
+
+    default:
+        break;
+    }
+}
+
+float ca_pid_f32(ca_pid_f32_t *pid,
+                 const float   ref,
+                 const float   set)
+{
+    /* Output */
+    float out = 0;
+    /* Input */
+    float in = set - ref;
+
+    switch (pid->mode)
+    {
+    case CA_PID_POSITION:
+    {
+        /*
+         When the limit of integration is exceeded and
+         the direction of integration is the same,
+         the integration stops
+        */
+        if ((-pid->omaxi < pid->x[2] &&
+             pid->x[2] < pid->omaxi) ||
+            pid->x[2] * in < 0)
+        {
+            pid->x[2] += pid->a[2] * in;
+        }
+
+        /* y[n] = a[0] * x[0] + a[1] * x[1] + a[2] * x[0] */
+        out = pid->a[0] * in;
+        out += pid->a[1] * pid->x[0];
+        out += pid->x[2];
+
+        /* Restrict the output of the PID */
+        out = LIMIT(out, pid->omin, pid->omax);
+
+        break;
+    }
+
+    case CA_PID_DELTA:
+    {
+        /* y[n] = y[n-1] + a[0] * x[0] + a[1] * x[1] + a[2] * x[2] */
+        pid->x[2] = pid->a[0] * in;
+        pid->x[2] += pid->a[1] * pid->x[0];
+        pid->x[2] += pid->a[2] * pid->x[1];
+
+        /* Restrict the output of the PID */
+        pid->x[2] = LIMIT(pid->x[2], pid->omin, pid->omax);
+        /* Export output */
+        out = pid->x[2];
+
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    /* Cache data */
+    pid->x[1] = pid->x[0];
+    pid->x[0] = in;
+
+    return out;
+}
+
+double ca_pid_f64(ca_pid_f64_t *pid,
+                  const double  ref,
+                  const double  set)
+{
+    /* Output */
+    double out = 0;
+    /* Input */
+    double in = set - ref;
+
+    switch (pid->mode)
+    {
+    case CA_PID_POSITION:
+    {
+        /*
+         When the limit of integration is exceeded and
+         the direction of integration is the same,
+         the integration stops
+        */
+        if ((-pid->omaxi < pid->x[2] &&
+             pid->x[2] < pid->omaxi) ||
+            pid->x[2] * in < 0)
+        {
+            pid->x[2] += pid->a[2] * in;
+        }
+
+        /* y[n] = a[0] * x[0] + a[1] * x[1] + a[2] * x[0] */
+        out = pid->a[0] * in;
+        out += pid->a[1] * pid->x[0];
+        out += pid->x[2];
+
+        /* Restrict the output of the PID */
+        out = LIMIT(out, pid->omin, pid->omax);
+
+        break;
+    }
+
+    case CA_PID_DELTA:
+    {
+        /* y[n] = y[n-1] + a[0] * x[0] + a[1] * x[1] + a[2] * x[2] */
+        pid->x[2] = pid->a[0] * in;
+        pid->x[2] += pid->a[1] * pid->x[0];
+        pid->x[2] += pid->a[2] * pid->x[1];
+
+        /* Restrict the output of the PID */
+        pid->x[2] = LIMIT(pid->x[2], pid->omin, pid->omax);
+        /* Export output */
+        out = pid->x[2];
+
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    /* Cache data */
+    pid->x[1] = pid->x[0];
+    pid->x[0] = in;
+
+    return out;
+}
+
+/* END OF FILE */
---
+++ ca-pid.h
@@ -0,0 +1,337 @@
+/*!
+ @file           ca-pid.h
+ @brief          Proportional Integral Derivative Algorithm
+ @details        A proportional–integral–derivative controller
+                 (PID controller or three-term controller) is a control loop
+                 mechanism employing feedback that is widely used in industrial
+                 control systems and a variety of other applications requiring
+                 continuously modulated control.
+                 - Position pid control
+ \f{aligned}
+    u(k) &= K_p e(k) + K_i \sum^k_{i=0} q e(i) + K_d [e(k) - e(k-1)]\\
+         &= (K_p + K_d) e(k) + (-K_d)e(k - 1) + K_i \sum^k_{i=0} q e(i)\\
+         &\begin{cases}q = 0 & |K_i \sum\limits^{k-1}_{i=0} e(i)| > E, e(k)e(k - 1) < 0 \\
+          q = 1\end{cases}
+ \f}
+                 - Incremental pid control
+ \f{aligned}
+    \Delta u(k) &= K_p [e(k) − e(k - 1)]
+                 + K_i e(k)
+                 + K_d [e(k) + e(k - 2) − 2 e(k - 1)] \\
+                &= (K_p + K_i + K_d) e(k)
+                 + (-K_p - 2 K_i) e(k - 1)
+                 + K_d e(k - 2)
+ \f}
+                 https://en.wikipedia.org/wiki/PID_controller
+ @author         tqfx tqfx@foxmail.com
+ @version        0
+ @date           2021-05-22
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+/* Define to prevent recursive inclusion */
+#ifndef __CA_PID_H__
+#define __CA_PID_H__
+
+#include "ca.h"
+
+#include <stdint.h>
+
+/*!
+ @ingroup        CA
+ @defgroup       CA_PID Proportional Integral Derivative
+ @{
+ @details        A proportional–integral–derivative controller
+                 (PID controller or three-term controller) is a control loop
+                 mechanism employing feedback that is widely used in industrial
+                 control systems and a variety of other applications requiring
+                 continuously modulated control.
+                 - Position pid control
+ \f{aligned}
+    u(k) &= K_p e(k) + K_i \sum^k_{i=0} q e(i) + K_d [e(k) - e(k-1)]\\
+         &= (K_p + K_d) e(k) + (-K_d)e(k - 1) + K_i \sum^k_{i=0} q e(i)\\
+         &\begin{cases}q = 0 & |K_i \sum\limits^{k-1}_{i=0} e(i)| > E, e(k)e(k - 1) < 0 \\
+          q = 1\end{cases}
+ \f}
+                 - Incremental pid control
+ \f{aligned}
+    \Delta u(k) &= K_p [e(k) − e(k - 1)]
+                 + K_i e(k)
+                 + K_d [e(k) + e(k - 2) − 2 e(k - 1)] \\
+                &= (K_p + K_i + K_d) e(k)
+                 + (-K_p - 2 K_i) e(k - 1)
+                 + K_d e(k - 2)
+ \f}
+                 https://en.wikipedia.org/wiki/PID_controller
+*/
+
+/*!
+ @enum           ca_pid_mode_t
+ @brief          Instance enumeration for PID Control Mode
+*/
+typedef enum
+{
+    CA_PID_POSITION,  //!< Position pid control
+    CA_PID_DELTA,     //!< Incremental pid control
+} ca_pid_mode_t;
+
+/*!
+ @struct         ca_pid_f32_t
+ @brief          Instance structure for the floating-point PID Control
+*/
+typedef struct
+{
+    ca_pid_mode_t mode;  //!< Mode of PID Control
+
+    float omin;   //!< Minimum output
+    float omax;   //!< Maximum output
+    float omaxi;  //!< Maximum intergral output
+
+    float a[3];  //!< Derived gain
+    /*!<
+     - @ref CA_PID_POSITION
+        - a[0] = Kp + Kd
+        - a[1] = - Kd
+        - a[2] = Ki
+     - @ref CA_PID_DELTA
+        - a[0] = Kp + Ki + Kd
+        - a[1] = -Kp - 2 * Kd
+        - a[2] = Kd
+    */
+    float x[3];  //!< State array
+    /*!<
+     - x[0] (last)
+     - x[1] (previous last)
+     - x[2] 
+        - @ref CA_PID_POSITION integral output
+        - @ref CA_PID_DELTA all output
+    */
+} ca_pid_f32_t;
+
+/*!
+ @struct         ca_pid_f64_t
+ @brief          Instance structure for the double floating-point PID Control
+*/
+typedef struct
+{
+    ca_pid_mode_t mode;  //!< Mode of PID Control
+
+    double omin;   //!< Minimum output
+    double omax;   //!< Maximum output
+    double omaxi;  //!< Maximum intergral output
+
+    double a[3];  //!< Derived gain
+    /*!<
+     - @ref CA_PID_POSITION
+        - a[0] = Kp + Kd
+        - a[1] = - Kd
+        - a[2] = Ki
+     - @ref CA_PID_DELTA
+        - a[0] = Kp + Ki + Kd
+        - a[1] = -Kp - 2 * Kd
+        - a[2] = Kd
+    */
+    double x[3];  //!< State array
+    /*!<
+     - x[0] (last)
+     - x[1] (previous last)
+     - x[2] 
+        - @ref CA_PID_POSITION integral output
+        - @ref CA_PID_DELTA all output
+    */
+} ca_pid_f64_t;
+
+__BEGIN_DECLS
+
+/*!
+ @brief          Initialization function for the floating-point PID Control
+ @param[in,out]  pid: An instance of the floating-point PID Control structure
+ @param[in]      mode: The mode of PID Control
+  @arg @ref      CA_PID_POSITION position pid control
+  @arg @ref      CA_PID_DELTA incremental pid control
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+ @param[in]      ...: Maximum intergral output
+*/
+extern void ca_pid_f32_init(ca_pid_f32_t *pid,
+                            ca_pid_mode_t mode,
+                            const float   k[3],
+                            const float   omin,
+                            const float   omax,
+                            ...);
+
+/*!
+ @brief          Initialization function for the double floating-point PID Control
+ @param[in,out]  pid: An instance of the double floating-point PID Control structure
+ @param[in]      mode: The mode of PID Control
+  @arg @ref      CA_PID_POSITION position pid control
+  @arg @ref      CA_PID_DELTA incremental pid control
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+ @param[in]      ...: Maximum intergral output
+*/
+extern void ca_pid_f64_init(ca_pid_f64_t *pid,
+                            ca_pid_mode_t mode,
+                            const double  k[3],
+                            const double  omin,
+                            const double  omax,
+                            ...);
+
+/*!
+ @brief          Process function for the floating-point PID Control
+ @param[in,out]  pid: An instance of the floating-point PID Control structure
+ @param[in]      ref: Reference point
+ @param[in]      set: Set point
+ @return         Output
+*/
+extern float ca_pid_f32(ca_pid_f32_t *pid,
+                        const float   ref,
+                        const float   set);
+
+/*!
+ @brief          Process function for the double floating-point PID Control
+ @param[in,out]  pid: An instance of the double floating-point PID Control structure
+ @param[in]      ref: Reference point
+ @param[in]      set: Set point
+ @return         Output
+*/
+extern float ca_pid_f64(ca_pid_f64_t *pid,
+                        const double  ref,
+                        const double  set);
+
+__END_DECLS
+
+__STATIC_INLINE
+/*!
+ @brief          Reset function for the floating-point PID Control
+ @param[in,out]  pid: An instance of the floating-point PID Control structure
+*/
+void ca_pid_f32_reset(ca_pid_f32_t *pid)
+{
+    /* Reset buffer */
+    pid->x[0] = pid->x[1] = pid->x[2] = 0;
+}
+
+__STATIC_INLINE
+/*!
+ @brief          Reset function for the double floating-point PID Control
+ @param[in,out]  pid: An instance of the double floating-point PID Control structure
+*/
+void ca_pid_f64_reset(ca_pid_f64_t *pid)
+{
+    /* Reset buffer */
+    pid->x[0] = pid->x[1] = pid->x[2] = 0;
+}
+
+__STATIC_INLINE
+/*!
+ @brief          Initialization function for the floating-point position PID Control
+ @param[in,out]  pid: An instance of the floating-point PID Control structure
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+ @param[in]      omaxi: Maximum intergral output
+*/
+void ca_pid_f32_position(ca_pid_f32_t *pid,
+                         const float   k[3],
+                         float         omin,
+                         float         omax,
+                         float         omaxi)
+{
+    ca_pid_f32_init(pid, CA_PID_POSITION, k, omin, omax, omaxi);
+}
+
+__STATIC_INLINE
+/*!
+ @brief          Initialization function for the floating-point incremental PID Control
+ @param[in,out]  pid: An instance of the floating-point PID Control structure
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+*/
+void ca_pid_f32_delta(ca_pid_f32_t *pid,
+                      const float   k[3],
+                      float         omin,
+                      float         omax)
+{
+    ca_pid_f32_init(pid, CA_PID_DELTA, k, omin, omax);
+}
+
+__STATIC_INLINE
+/*!
+ @brief          Initialization function for the double floating-point position PID Control
+ @param[in,out]  pid: An instance of the double floating-point PID Control structure
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+ @param[in]      omaxi: Maximum intergral output
+*/
+void ca_pid_f64_position(ca_pid_f64_t *pid,
+                         const double  k[3],
+                         const double  omin,
+                         const double  omax,
+                         const double  omaxi)
+{
+    ca_pid_f64_init(pid, CA_PID_POSITION, k, omin, omax, omaxi);
+}
+
+__STATIC_INLINE
+/*!
+ @brief          Initialization function for the double floating-point incremental PID Control
+ @param[in,out]  pid: An instance of the double floating-point PID Control structure
+ @param[in]      k: constant array
+  @arg           k[0] (Proportional constant)
+  @arg           k[1] (Integral constant)
+  @arg           k[2] (Derivative constant)
+ @param[in]      omin: Minimum output
+ @param[in]      omax: Maximum output
+*/
+void ca_pid_f64_delta(ca_pid_f64_t *pid,
+                      const double  k[3],
+                      const double  omin,
+                      const double  omax)
+{
+    ca_pid_f64_init(pid, CA_PID_DELTA, k, omin, omax);
+}
+
+/*!< @} */
+
+/* Terminate definition to prevent recursive inclusion */
+#endif /* __CA_PID_H__ */
+
+/*!********************** (C) COPYRIGHT TQFX *******************END OF FILE****/
