536905b3ef2466b97320e7668ed0cbcbfea5227c
tqfx <tqfx@foxmail.com>
2021-05-22 18:37:55 +0800

    Add mianpage of doxygen

--- ca.c
+++ ca.c
@@ -1,34 +1,74 @@
-/**
- * *****************************************************************************
- * @file         ca.c/h
- * @brief        Algorithm library
- * @author       tqfx
- * @date         20210430
- * @version      1
- * @copyright    Copyright (c) 2021
- * @code         utf-8                                                  @endcode
- * *****************************************************************************
+/*!
+ @mainpage       Algorithm library
+ @brief          Algorithm library
+ @author         tqfx tqfx@foxmail.com
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 */
 
-/* Includes ------------------------------------------------------------------*/
+/*!
+ @file           ca.h
+ @brief          Algorithm library
+ @author         tqfx tqfx@foxmail.com
+ @version        0
+ @date           2021-05-22
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+/* Includes */
 #include "ca.h"
 
-/* Private includes ----------------------------------------------------------*/
+/* Sources */
 
-/**
- * @brief          Computational algorithm
+/*!
+ @brief          Computational algorithm
 */
 #include "ca_math.c"
 
-/**
- * @brief          Control algorithm
+/*!
+ @brief          Control algorithm
 */
 #include "ca_pid.c"
 
-/**
- * @brief          Conversion algorithm
+/*!
+ @brief          Conversion algorithm
 */
 #include "ahrs.c"
 #include "zyx.c"
 
-/************************ (C) COPYRIGHT TQFX *******************END OF FILE****/
+/* END OF FILE */
