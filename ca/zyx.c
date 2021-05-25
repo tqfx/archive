001a51c58c6f381a093f6f6f413aef85a8ad27d5
tqfx <tqfx@foxmail.com>
2021-05-25 22:25:46 +0800

    Update zyx

--- ca/zyx.c
+++ ca/zyx.c
@@ -1,40 +1,45 @@
-/**
- * *****************************************************************************
- * @file         zyx.c/h
- * @brief        yaw (Z), pitch (Y), roll (X)
- * @details      where the X-axis points forward,
- *               Y-axis to the right and Z-axis downward
- * @author       tqfx
- * @date         20210430
- * @version      1
- * @copyright    Copyright (C) 2021
- * @code         utf-8                                                  @endcode
- * *****************************************************************************
+/*!
+ @file           zyx.c
+ @brief          yaw (Z), pitch (Y), roll (X)
+ @details        where the X-axis points forward,
+                 Y-axis to the right and Z-axis downward
+ @author         tqfx tqfx@foxmail.com
+ @version        0
+ @date           2021-05-25
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 */
 
-/* Includes ------------------------------------------------------------------*/
 #include "zyx.h"
 
-/* Private includes ----------------------------------------------------------*/
 #include <math.h>
-
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private typedef -----------------------------------------------------------*/
-/* Private types -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private user code ---------------------------------------------------------*/
 
 void zyx_euler_quat(const double e[3], double q[4])
 {
     /* Abbreviations for the various angular functions */
-    double cy = cos(e[0] * 0.5);
-    double sy = sin(e[0] * 0.5);
-    double cp = cos(e[1] * 0.5);
-    double sp = sin(e[1] * 0.5);
-    double cr = cos(e[2] * 0.5);
-    double sr = sin(e[2] * 0.5);
+    double cy = cos(e[ZYX_YAW] * 0.5);
+    double sy = sin(e[ZYX_YAW] * 0.5);
+    double cp = cos(e[ZYX_PITCH] * 0.5);
+    double sp = sin(e[ZYX_PITCH] * 0.5);
+    double cr = cos(e[ZYX_ROLL] * 0.5);
+    double sr = sin(e[ZYX_ROLL] * 0.5);
 
     q[0] = cr * cp * cy + sr * sp * sy;
     q[1] = sr * cp * cy - cr * sp * sy;
@@ -45,12 +50,12 @@ void zyx_euler_quat(const double e[3], double q[4])
 void zyx_f32_euler_quat(const float e[3], float q[4])
 {
     /* Abbreviations for the various angular functions */
-    float cy = cos(e[0] * 0.5F);
-    float sy = sin(e[0] * 0.5F);
-    float cp = cos(e[1] * 0.5F);
-    float sp = sin(e[1] * 0.5F);
-    float cr = cos(e[2] * 0.5F);
-    float sr = sin(e[2] * 0.5F);
+    float cy = cosf(e[ZYX_YAW] * 0.5F);
+    float sy = sinf(e[ZYX_YAW] * 0.5F);
+    float cp = cosf(e[ZYX_PITCH] * 0.5F);
+    float sp = sinf(e[ZYX_PITCH] * 0.5F);
+    float cr = cosf(e[ZYX_ROLL] * 0.5F);
+    float sr = sinf(e[ZYX_ROLL] * 0.5F);
 
     q[0] = cr * cp * cy + sr * sp * sy;
     q[1] = sr * cp * cy - cr * sp * sy;
@@ -64,25 +69,25 @@ void zyx_quat_euler(const double q[4], double e[3])
     double sinr_cosp = 2 * (q[0] * q[1] + q[2] * q[3]);
     double cosr_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
 
-    e[2] = atan2(sinr_cosp, cosr_cosp);
+    e[ZYX_ROLL] = atan2(sinr_cosp, cosr_cosp);
 
     /* pitch (y-axis rotation) */
     double sinp = 2 * (q[0] * q[2] - q[3] * q[1]);
     if (fabs(sinp) >= 1)
     {
         /* use 90 degrees if out of range */
-        e[1] = copysign(M_PI / 2, sinp);
+        e[ZYX_PITCH] = copysign(M_PI / 2, sinp);
     }
     else
     {
-        e[1] = asin(sinp);
+        e[ZYX_PITCH] = asin(sinp);
     }
 
     /* yaw (z-axis rotation) */
     double siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2]);
     double cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3]);
 
-    e[0] = atan2(siny_cosp, cosy_cosp);
+    e[ZYX_YAW] = atan2(siny_cosp, cosy_cosp);
 }
 
 void zyx_f32_quat_euler(const float q[4], float e[3])
@@ -91,25 +96,25 @@ void zyx_f32_quat_euler(const float q[4], float e[3])
     float sinr_cosp = 2 * (q[0] * q[1] + q[2] * q[3]);
     float cosr_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
 
-    e[2] = atan2f(sinr_cosp, cosr_cosp);
+    e[ZYX_ROLL] = atan2f(sinr_cosp, cosr_cosp);
 
     /* pitch (y-axis rotation) */
     float sinp = 2 * (q[0] * q[2] - q[3] * q[1]);
-    if (fabs(sinp) >= 1)
+    if (fabsf(sinp) >= 1)
     {
         /* use 90 degrees if out of range */
-        e[1] = copysignf(M_PI / 2, sinp);
+        e[ZYX_PITCH] = copysignf((float)(M_PI / 2), sinp);
     }
     else
     {
-        e[1] = asinf(sinp);
+        e[ZYX_PITCH] = asinf(sinp);
     }
 
     /* yaw (z-axis rotation) */
-    double siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2]);
-    double cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3]);
+    float siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2]);
+    float cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3]);
 
-    e[0] = atan2f(siny_cosp, cosy_cosp);
+    e[ZYX_YAW] = atan2f(siny_cosp, cosy_cosp);
 }
 
-/************************ (C) COPYRIGHT TQFX *******************END OF FILE****/
+/* END OF FILE */
--- ca/zyx.h
+++ ca/zyx.h
@@ -1,68 +1,90 @@
-/**
- * *****************************************************************************
- * @file         zyx.c/h
- * @brief        yaw (Z), pitch (Y), roll (X)
- * @details      where the X-axis points forward,
- *               Y-axis to the right and Z-axis downward
- * @author       tqfx
- * @date         20210430
- * @version      1
- * @copyright    Copyright (C) 2021
- * @code         utf-8                                                  @endcode
- * *****************************************************************************
+/*!
+ @file           zyx.h
+ @brief          yaw (Z), pitch (Y), roll (X)
+ @details        where the X-axis points forward,
+                 Y-axis to the right and Z-axis downward
+ @author         tqfx tqfx@foxmail.com
+ @version        0
+ @date           2021-05-25
+ @copyright      Copyright (C) 2021 tqfx
+ \n \n
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ \n \n
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ \n \n
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
+/* Define to prevent recursive inclusion */
 #ifndef __ZYX_H__
 #define __ZYX_H__
 
-/* Includes ------------------------------------------------------------------*/
 #include "ca.h"
 
-/* Exported constants --------------------------------------------------------*/
+/*!
+ @ingroup        CA
+ @defgroup       CA_ZYX zyx axis
+ @{
+*/
 
 #undef ZYX_YAW
 #undef ZYX_PITCH
 #undef ZYX_ROLL
-#define ZYX_YAW   0U /* Z (yaw) */
-#define ZYX_PITCH 1U /* Y (pitch) */
-#define ZYX_ROLL  2U /* X (roll) */
-
-/* Exported functions prototypes ---------------------------------------------*/
+#define ZYX_YAW   0U  //!< Z (yaw)
+#define ZYX_PITCH 1U  //!< Y (pitch)
+#define ZYX_ROLL  2U  //!< X (roll)
 
 __BEGIN_DECLS
 
-/**
- * @brief        euler angles to quaternion conversion by ZYX
- * @param[in]    e[3]: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
- * @param[out]   q[4]: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
+/*!
+ @brief        euler angles to quaternion conversion by ZYX
+ @param[in]    e: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
+ @param[out]   q: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
 */
-extern void zyx_euler_quat(const double e[3], double q[4]);
+extern void zyx_euler_quat(const double e[3],
+                           double       q[4]);
 
-/**
- * @brief        euler angles to quaternion conversion by ZYX
- * @param[in]    e[3]: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
- * @param[out]   q[4]: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
+/*!
+ @brief        euler angles to quaternion conversion by ZYX
+ @param[in]    e: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
+ @param[out]   q: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
 */
-extern void zyx_f32_euler_quat(const float e[3], float q[4]);
+extern void zyx_f32_euler_quat(const float e[3],
+                               float       q[4]);
 
-/**
- * @brief        quaternion to euler angles conversion by ZYX
- * @param[in]    q[4]: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
- * @param[out]   e[3]: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
+/*!
+ @brief        quaternion to euler angles conversion by ZYX
+ @param[in]    q: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
+ @param[out]   e: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
 */
-extern void zyx_quat_euler(const double q[4], double e[3]);
+extern void zyx_quat_euler(const double q[4],
+                           double       e[3]);
 
-/**
- * @brief        quaternion to euler angles conversion by ZYX
- * @param[in]    q[4]: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
- * @param[out]   e[3]: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
+/*!
+ @brief        quaternion to euler angles conversion by ZYX
+ @param[in]    q: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
+ @param[out]   e: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
 */
-extern void zyx_f32_quat_euler(const float q[4], float e[3]);
+extern void zyx_f32_quat_euler(const float q[4],
+                               float       e[3]);
 
 __END_DECLS
 
-/* Terminate definition to prevent recursive inclusion -----------------------*/
+/*! @} */
+
+/* Enddef to prevent recursive inclusion */
 #endif /* __ZYX_H__ */
 
-/************************ (C) COPYRIGHT TQFX *******************END OF FILE****/
+/* END OF FILE */
