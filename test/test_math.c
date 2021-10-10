a26e974e9726db988a51660398f7e0fc93da2811
tqfx <tqfx@foxmail.com>
2021-10-10 12:28:10 +0800

    test(math): better format

--- test/test_math.c
+++ test/test_math.c
@@ -53,22 +53,22 @@ int main(void)
     for (unsigned int i = 0; i != sizeof(datat) / sizeof(double); ++i)
     {
         b32->f32 = (float)datat[i];
-        printf("%+f\t0x%lX\t%u,0x%02X,0x%06X\n",
-               (double)b32->f32,
+        printf("0x%lX    %u,0x%02X,0x%06X    %+f\n",
                b32->u32,
                b32->s32->s,
                b32->s32->e,
-               b32->s32->f);
+               b32->s32->f,
+               (double)b32->f32);
     }
     for (unsigned int i = 0; i != sizeof(datat) / sizeof(double); ++i)
     {
         b64->f64 = datat[i];
-        printf("%+f\t0x%llX\t%u,0x%03X,0x%014llX\n",
-               b64->f64,
+        printf("0x%llX    %u,0x%03X,0x%014llX    %+f\n",
                b64->u64,
                (unsigned int)b64->s64->s,
                (unsigned int)b64->s64->e,
-               (unsigned long long)b64->s64->f);
+               (unsigned long long)b64->s64->f,
+               b64->f64);
     }
 
     static float datasqrt[] = {
@@ -82,13 +82,13 @@ int main(void)
 
     for (unsigned int i = 0; i != sizeof(datasqrt) / sizeof(float); ++i)
     {
-        printf("1/sqrt(%g):\t%g\t%g\n",
+        printf("1/sqrt(%g):\t%-10g%-10g\n",
                (double)datasqrt[i],
                1 / (double)sqrtf(datasqrt[i]),
                (double)a_inv_sqrt(datasqrt[i]));
     }
 
-    printf("%lu\t%llu\n", a_sqrt_u32(4), a_sqrt_u64(4));
+    printf("a_sqrt(4):\t%-10lu%-10llu\n", a_sqrt_u32(4), a_sqrt_u64(4));
 
     return 0;
 }
