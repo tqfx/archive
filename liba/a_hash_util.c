81090be702288448e1ae079a06415d3a63dc41b4
tqfx <tqfx@foxmail.com>
2021-10-13 00:26:07 +0800

    feat(hash): a_hash_util

---
+++ liba/a_hash_util.c
@@ -0,0 +1,127 @@
+/*!
+ @file           a_hash_util.c
+ @brief          hash library utils
+ @author         tqfx tqfx@foxmail.com
+ @copyright      Copyright (C) 2020 tqfx
+*/
+
+#include "a_hash_util.h"
+
+#include <stdarg.h>
+
+int a_hash_memory(const a_hash_t *ctx, const void *p, size_t n, void *out, size_t *siz)
+{
+    /* assert(ctx) */
+    /* assert(out) */
+    /* assert(siz) */
+    /* assert(!n || p) */
+    if (*siz < ctx->outsiz)
+    {
+        *siz = ctx->outsiz;
+        return A_HASH_OVERFLOW;
+    }
+
+    a_hash_stat_t stat[1];
+
+    ctx->init(stat);
+    if (ctx->process(stat, p, n) != A_HASH_SUCCESS)
+    {
+        return A_HASH_FAILURE;
+    }
+    *siz = (ctx->done(stat, out) ? ctx->outsiz : 0);
+
+    return A_HASH_SUCCESS;
+}
+
+int a_hash_memory_multi(const a_hash_t *ctx, void *out, size_t *siz, const void *p, size_t n, ...)
+{
+    /* assert(ctx) */
+    /* assert(out) */
+    /* assert(siz) */
+    /* assert(!n || p) */
+    if (*siz < ctx->outsiz)
+    {
+        *siz = ctx->outsiz;
+        return A_HASH_OVERFLOW;
+    }
+
+    va_list arg;
+    va_start(arg, n);
+    a_hash_stat_t stat[1];
+
+    ctx->init(stat);
+    for (;;)
+    {
+        if (ctx->process(stat, p, n) != A_HASH_SUCCESS)
+        {
+            va_end(arg);
+            return A_HASH_FAILURE;
+        }
+        p = va_arg(arg, const void *);
+        if (0 == p)
+        {
+            va_end(arg);
+            break;
+        }
+        n = va_arg(arg, size_t);
+    };
+    *siz = (ctx->done(stat, out) ? ctx->outsiz : 0);
+
+    return A_HASH_SUCCESS;
+}
+
+int a_hash_filehandle(const a_hash_t *ctx, FILE *in, void *out, size_t *siz)
+{
+    /* assert(in) */
+    /* assert(ctx) */
+    /* assert(out) */
+    /* assert(siz) */
+    if (*siz < ctx->outsiz)
+    {
+        *siz = ctx->outsiz;
+        return A_HASH_OVERFLOW;
+    }
+
+    a_hash_stat_t stat[1];
+
+    char buf[BUFSIZ];
+    ctx->init(stat);
+    do
+    {
+        *siz = fread(buf, 1, BUFSIZ, in);
+        ctx->process(stat, buf, *siz);
+    } while (*siz == BUFSIZ);
+    *siz = (ctx->done(stat, out) ? ctx->outsiz : 0);
+
+    return A_HASH_SUCCESS;
+}
+
+int a_hash_file(const a_hash_t *ctx, const char *fname, void *out, size_t *siz)
+{
+    /* assert(ctx) */
+    /* assert(out) */
+    /* assert(siz) */
+    /* assert(fname) */
+    if (*siz < ctx->outsiz)
+    {
+        *siz = ctx->outsiz;
+        return A_HASH_OVERFLOW;
+    }
+
+    FILE *in = fopen(fname, "rb");
+    if (0 == in)
+    {
+        return A_HASH_NOTFOUND;
+    }
+
+    int ret = a_hash_filehandle(ctx, in, out, siz);
+
+    if (fclose(in))
+    {
+        return A_HASH_FAILURE;
+    }
+
+    return ret;
+}
+
+/* END OF FILE */
---
+++ liba/a_hash_util.h
@@ -0,0 +1,70 @@
+/*!
+ @file           a_hash_util.h
+ @brief          hash library utils
+ @author         tqfx tqfx@foxmail.com
+ @copyright      Copyright (C) 2020 tqfx
+*/
+
+/* Define to prevent recursive inclusion */
+#ifndef __A_HASH_UTIL_H__
+#define __A_HASH_UTIL_H__
+
+#include "a_hash.h"
+
+#include <stdio.h>
+
+__BEGIN_DECLS
+
+/*!
+ @brief          Hash a block of memory and store the digest.
+ @param[in]      ctx: points to an instance of hash.
+ @param[in]      p: points to data to hash.
+ @param[in]      n: length of data to hash.
+ @param[out]     out: where to store the digest.
+ @param[in,out]  siz: max size and resulting size of the digest.
+ @return         the execution state of the function.
+  @retval        0 success
+*/
+extern int a_hash_memory(const a_hash_t *ctx, const void *p, size_t n, void *out, size_t *siz);
+
+/*!
+ @brief          Hash a block of memory and store the digest.
+ @param[in]      ctx: points to an instance of hash.
+ @param[out]     out: where to store the digest.
+ @param[in,out]  siz: max size and resulting size of the digest.
+ @param[in]      p: points to data to hash.
+ @param[in]      n: length of data to hash.
+ @param[in]      ...: tuples of (p,n) pairs to hash, terminated with a (NULL,x) (x=don't care)
+ @return         the execution state of the function.
+  @retval        0 success
+*/
+extern int a_hash_memory_multi(const a_hash_t *ctx, void *out, size_t *siz, const void *p, size_t n, ...);
+
+/*!
+ @brief          Hash data from an open file handle.
+ @param[in]      ctx: points to an instance of hash.
+ @param[in]      in: points to FILE handle to hash.
+ @param[out]     out: where to store the digest.
+ @param[in,out]  siz: max size and resulting size of the digest.
+ @return         the execution state of the function.
+  @retval        0 success
+*/
+extern int a_hash_filehandle(const a_hash_t *ctx, FILE *in, void *out, size_t *siz);
+
+/*!
+ @brief          Hash a block of memory and store the digest.
+ @param[in]      ctx: points to an instance of hash.
+ @param[in]      fname: name of file to hash.
+ @param[out]     out: where to store the digest.
+ @param[in,out]  siz: max size and resulting size of the digest.
+ @return         the execution state of the function.
+  @retval        0 success
+*/
+extern int a_hash_file(const a_hash_t *ctx, const char *fname, void *out, size_t *siz);
+
+__END_DECLS
+
+/* Enddef to prevent recursive inclusion */
+#endif /* __A_HASH_UTIL_H__ */
+
+/* END OF FILE */
