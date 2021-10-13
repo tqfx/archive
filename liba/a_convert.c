d18e799dbf25e3073916754d4a21b12aa0881733
tqfx <tqfx@foxmail.com>
2021-10-13 13:24:07 +0800

    feat(convert): lower upper

--- liba/a_convert.c
+++ liba/a_convert.c
@@ -7,6 +7,8 @@
 
 #include "a_convert.h"
 
+#include <ctype.h>
+
 void *a_digest(const void *p, size_t n, void *out)
 {
     /* assert(!n || p) */
@@ -22,6 +24,44 @@ void *a_digest(const void *p, size_t n, void *out)
             *o++ = hexits[*s >> 0x4];
             *o++ = hexits[*s & 0x0F];
             ++s;
+        }
+        *o = 0;
+    }
+
+    return out;
+}
+
+void *a_lower(const void *p, size_t n, void *out)
+{
+    /* assert(!n || p) */
+    const char *s = (const char *)p;
+
+    if (out || (out = a_alloc(n + 1), out))
+    {
+        char *o = (char *)out;
+        p = (const void *)(s + n);
+        while (s != p)
+        {
+            *o++ = (char)tolower(*s++);
+        }
+        *o = 0;
+    }
+
+    return out;
+}
+
+void *a_upper(const void *p, size_t n, void *out)
+{
+    /* assert(!n || p) */
+    const char *s = (const char *)p;
+
+    if (out || (out = a_alloc(n + 1), out))
+    {
+        char *o = (char *)out;
+        p = (const void *)(s + n);
+        while (s != p)
+        {
+            *o++ = (char)toupper(*s++);
         }
         *o = 0;
     }
--- liba/a_convert.h
+++ liba/a_convert.h
@@ -17,13 +17,33 @@ __BEGIN_DECLS
 
 /*!
  @brief          Convert a digest to a string.
- @param[in]      p: points to an array that holds the digest.
- @param[in]      n: integer defining the lengs of the output.
+ @param[in]      p: points to data to convert.
+ @param[in]      n: length of data to convert.
  @param[in,out]  out: points to buffer that holds the string.
  @return         a pointer to the string.
  @note           When out is 0, you need to use @ref a_free to release the memory.
 */
 extern void *a_digest(const void *p, size_t n, void *out);
+
+/*!
+ @brief          Convert a string to a lower string.
+ @param[in]      p: points to data to convert.
+ @param[in]      n: length of data to convert.
+ @param[in,out]  out: points to buffer that holds the string.
+ @return         a pointer to the string.
+ @note           When out is 0, you need to use @ref a_free to release the memory.
+*/
+extern void *a_lower(const void *p, size_t n, void *out);
+
+/*!
+ @brief          Convert a string to a upper string.
+ @param[in]      p: points to data to convert.
+ @param[in]      n: length of data to convert.
+ @param[in,out]  out: points to buffer that holds the string.
+ @return         a pointer to the string.
+ @note           When out is 0, you need to use @ref a_free to release the memory.
+*/
+extern void *a_upper(const void *p, size_t n, void *out);
 
 __END_DECLS
 
