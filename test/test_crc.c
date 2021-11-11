53d4add3d389455373867a75201e7ea3747d252b
tqfx <tqfx@foxmail.com>
2021-11-11 15:04:33 +0800

    test: add crc print

--- test/test_crc.c
+++ test/test_crc.c
@@ -1,6 +1,6 @@
 /*!
- @file test_crctab.c
- @brief Create Cyclic Redundancy Check Table
+ @file test_crc.c
+ @brief test Cyclic Redundancy Check
  @copyright Copyright (C) 2020 tqfx. All rights reserved.
 */
 
@@ -10,17 +10,17 @@
 #include <stdlib.h>
 
 #undef __WRITE_TAB
-#define __WRITE_TAB(_bit, _)                                                                \
+#define __WRITE_TAB(_bit, _row, _fmt)                                                       \
     void write_tab##_bit(FILE *_out, uint##_bit##_t _tab[A_CRC_TABSIZ], const char *_label) \
     {                                                                                       \
         fprintf(_out, "const uint%u_t %s[0x%X] = {\n", _bit, _label, A_CRC_TABSIZ);         \
         fprintf(_out, "    /* clang-format off */\n");                                      \
-        for (size_t i = 0; i != A_CRC_TABSIZ / 8; ++i)                                      \
+        for (size_t i = 0; i != A_CRC_TABSIZ / _row; ++i)                                   \
         {                                                                                   \
             fprintf(_out, "    ");                                                          \
-            for (size_t j = 0; j != 8; ++j)                                                 \
+            for (size_t j = 0; j != _row; ++j)                                              \
             {                                                                               \
-                fprintf(_out, "0x%0" #_ "X,", _tab[8 * i + j]);                             \
+                fprintf(_out, "0x%0" _fmt "X,", _tab[_row * i + j]);                        \
                 if (j != 7)                                                                 \
                 {                                                                           \
                     fprintf(_out, " ");                                                     \
@@ -31,31 +31,11 @@
         fprintf(_out, "    /* clang-format on */\n");                                       \
         fprintf(_out, "};\n\n");                                                            \
     }
-__WRITE_TAB(8, 2)
-__WRITE_TAB(16, 4)
-__WRITE_TAB(32, 8)
+__WRITE_TAB(8, 8, "2")
+__WRITE_TAB(16, 8, "4")
+__WRITE_TAB(32, 8, "8")
+__WRITE_TAB(64, 4, "16z")
 #undef __WRITE_TAB
-
-void write_tab64(FILE *_out, uint64_t _tab[A_CRC_TABSIZ], const char *_label)
-{
-    fprintf(_out, "const uint%u_t %s[0x%X] = {\n", 64, _label, A_CRC_TABSIZ);
-    fprintf(_out, "    /* clang-format off */\n");
-    for (size_t i = 0; i != A_CRC_TABSIZ / 4; ++i)
-    {
-        fprintf(_out, "    ");
-        for (size_t j = 0; j != 4; ++j)
-        {
-            fprintf(_out, "0x%016zX,", _tab[4 * i + j]);
-            if (j != 3)
-            {
-                fprintf(_out, " ");
-            }
-        }
-        fprintf(_out, "\n");
-    }
-    fprintf(_out, "    /* clang-format on */\n");
-    fprintf(_out, "};\n\n");
-}
 
 void create_table(const char *_fname)
 {
@@ -111,16 +91,52 @@ void create_table(const char *_fname)
     fclose(fp);
 }
 
+void test(void)
+{
+    const char *text = "123456789";
+    unsigned int size = sizeof("123456789") - 1;
+
+    uint8_t tab8[A_CRC_TABSIZ];
+    printf("POLY: 0x%02X\n", A_CRC8_POLY);
+    printf("INIT: 0x%02X\n", A_CRC8_INIT);
+    a_crc8_lsb(tab8, A_CRC8_POLY);
+    printf("LSB: 0x%02X\n", a_crc8(tab8, text, size, A_CRC8_INIT));
+    a_crc8_msb(tab8, A_CRC8_POLY);
+    printf("MSB: 0x%02X\n", a_crc8(tab8, text, size, A_CRC8_INIT));
+
+    uint16_t tab16[A_CRC_TABSIZ];
+    printf("POLY: 0x%04X\n", A_CRC16_POLY);
+    printf("INIT: 0x%04X\n", A_CRC16_INIT);
+    a_crc16_lsb(tab16, A_CRC16_POLY);
+    printf("LSB: 0x%04X\n", a_crc16(tab16, text, size, A_CRC16_INIT));
+    a_crc16_msb(tab16, A_CRC16_POLY);
+    printf("MSB: 0x%04X\n", a_crc16(tab16, text, size, A_CRC16_INIT));
+
+    uint32_t tab32[A_CRC_TABSIZ];
+    printf("POLY: 0x%08X\n", A_CRC32_POLY);
+    printf("INIT: 0x%08X\n", A_CRC32_INIT);
+    a_crc32_lsb(tab32, A_CRC32_POLY);
+    printf("LSB: 0x%08X\n", a_crc32(tab32, text, size, A_CRC32_INIT));
+    a_crc32_msb(tab32, A_CRC32_POLY);
+    printf("MSB: 0x%08X\n", a_crc32(tab32, text, size, A_CRC32_INIT));
+
+    uint64_t tab64[A_CRC_TABSIZ];
+    printf("POLY: 0x%016zX\n", A_CRC64_POLY);
+    printf("INIT: 0x%016zX\n", A_CRC64_INIT);
+    a_crc64_lsb(tab64, A_CRC64_POLY);
+    printf("LSB: 0x%016zX\n", a_crc64(tab64, text, size, A_CRC64_INIT));
+    a_crc64_msb(tab64, A_CRC64_POLY);
+    printf("MSB: 0x%016zX\n", a_crc64(tab64, text, size, A_CRC64_INIT));
+}
+
 int main(int argc, char *argv[])
 {
-    const char *fname = 0;
-
     if (argc > 1)
     {
-        fname = argv[argc - 1];
+        create_table(argv[argc - 1]);
     }
 
-    create_table(fname);
+    test();
 
     return 0;
 }
