7a1d4b1d8d5720ff72024d7413efa6a49cf0a524
tqfx <tqfx@foxmail.com>
2021-11-14 21:52:30 +0800

    fix(zyc): a_zyx*_euler2quat

--- liba/a_zyx.h
+++ liba/a_zyx.h
@@ -25,8 +25,8 @@ __BEGIN_DECLS
  @param[in] e: 0 (yaw), 1 (pitch), 2 (roll), unit /rad
  @param[out] q: 0 (w q0), 1 (x q1), 2 (y q2), 3 (z q3)
 */
-extern void a_zyx_euler_quat(const double e[3], double q[4]) __NONNULL_ALL;
-extern void a_zyxf_euler_quat(const float e[3], float q[4]) __NONNULL_ALL;
+extern void a_zyx_euler2quat(const double e[3], double q[4]) __NONNULL_ALL;
+extern void a_zyxf_euler2quat(const float e[3], float q[4]) __NONNULL_ALL;
 
 /*!
  @brief quaternion to euler angles conversion by ZYX
