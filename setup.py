f573fbe629a0a48402b9ab72ee82e0288c56917a
tqfx <tqfx@foxmail.com>
2022-05-06 22:25:02 +0800

    fix(setup.py): distutils package is deprecated
    The distutils package is deprecated and slated for removal in Python 3.12.
    Use setuptools or check PEP 632 for potential alternatives

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ script = os.path.abspath(argv[0])
 os.chdir(os.path.dirname(script))
 if len(argv) < 2:
     exit(Popen([executable, script, "build_ext", "--inplace"]).wait())
-from distutils.ccompiler import get_default_compiler
+from setuptools.command.build_ext import build_ext
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 from glob import glob
@@ -30,37 +30,46 @@ suffix_cy = (".pyx", ".py")
 
 sources = []
 sources_cy = []
-include_dirs = ["include"]
-define_macros = [("a_VERSION", '"%s"' % (version))]
 for source in glob("src/**", recursive=True):
     if not os.path.isfile(source):
         continue
     prefix, suffix = os.path.splitext(source)
-    if suffix in suffix_cy:
-        sources_cy.append(source)
-        for fix in suffix_cc:
-            if os.path.exists(prefix + fix):
-                os.remove(prefix + fix)
-    if suffix in suffix_cc + suffix_cy and os.path.exists(source):
-        sources.append(source)
-link_args = []
-compile_args = []
-if get_default_compiler() == "mingw32":
-    link_args += [
-        "-static-libgcc",
-        "-static-libstdc++",
-        "-Wl,-Bstatic,--whole-archive",
-        "-lwinpthread",
-        "-Wl,--no-whole-archive",
-    ]
+    if suffix not in suffix_cy:
+        continue
+    sources.append(source)
+    sources_cy.append(source)
+    for suffix in suffix_cc:
+        if os.path.exists(prefix + suffix):
+            os.remove(prefix + suffix)
+for source in glob("src/**", recursive=True):
+    if not os.path.isfile(source):
+        continue
+    prefix, suffix = os.path.splitext(source)
+    if suffix not in suffix_cc:
+        continue
+    sources.append(source)
 ext_modules = Extension(
     name=name,
     sources=sources,
-    include_dirs=include_dirs,
-    define_macros=define_macros,
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
+    include_dirs=["include"],
+    define_macros=[("a_VERSION", '"%s"' % (version))],
 )
+
+
+class Build(build_ext):
+    def build_extensions(self):
+        if self.compiler.compiler_type == "mingw32":
+            for e in self.extensions:
+                e.extra_link_args = [
+                    "-static-libgcc",
+                    "-static-libstdc++",
+                    "-Wl,-Bstatic,--whole-archive",
+                    "-lwinpthread",
+                    "-Wl,--no-whole-archive",
+                ]
+        super(Build, self).build_extensions()
+
+
 try:
     setup(
         name=name,
@@ -70,6 +79,7 @@ try:
         author_email=author_email,
         url=url,
         license=license,
+        cmdclass={"build_ext": Build},
         ext_modules=cythonize(
             ext_modules,
             language_level=3,
@@ -79,8 +89,9 @@ try:
     )
 except Exception as e:
     print(e)
-for pyx in sources_cy:
-    prefix = os.path.splitext(pyx)[0]
+
+for source in sources_cy:
+    prefix, suffix = os.path.splitext(source)
     for suffix in suffix_cc:
         if os.path.exists(prefix + suffix):
             os.remove(prefix + suffix)
