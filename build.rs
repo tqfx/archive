bf4af65b6b3d39691b3f0416d622344124a87513
tqfx <tqfx@foxmail.com>
2022-06-10 18:38:08 +0800

    fix(rust): rustc-link

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
 use cmake::Config;
 
 fn main() {
-    let mut config = Config::new(".");
+    let mut config = Config::new("");
 
     config.define("BUILD_TESTING", "0");
-
     config.static_crt(true);
 
-    let dst = config.build();
+    let out = config.build();
+    let lib = out.join("lib");
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=a");
+    println!("cargo:rustc-link-search=native={}", lib.display());
+    println!("cargo:rustc-link-lib=static={}", "a");
 }
