219ac940c364ef539f9356dd949a8252cca75e29
tqfx <tqfx@foxmail.com>
2022-06-24 01:29:38 +0800

    docs(doxygen): update configuration

--- include/a/misc/math.h
+++ include/a/misc/math.h
@@ -20,19 +20,19 @@
 #define A_FLT_F(func, ...)  func##f(__VA_ARGS__)
 #define A_LDBL_F(func, ...) func##l(__VA_ARGS__)
 
-#define A_E        2.71828182845904523536  // e
-#define A_LOG2E    1.44269504088896340736  // log2(e)
-#define A_LOG10E   0.434294481903251827651 // log10(e)
-#define A_LN2      0.693147180559945309417 // ln(2)
-#define A_LN10     2.30258509299404568402  // ln(10)
-#define A_PI       3.14159265358979323846  // pi
-#define A_PI_2     1.57079632679489661923  // pi/2
-#define A_PI_4     0.785398163397448309616 // pi/4
-#define A_1_PI     0.318309886183790671538 // 1/pi
-#define A_2_PI     0.636619772367581343076 // 2/pi
-#define A_2_SQRTPI 1.12837916709551257390  // 2/sqrt(pi)
-#define A_SQRT2    1.41421356237309504880  // sqrt(2)
-#define A_SQRT1_2  0.707106781186547524401 // 1/sqrt(2)
+#define A_E        2.71828182845904523536  //!< e
+#define A_LOG2E    1.44269504088896340736  //!< log2(e)
+#define A_LOG10E   0.434294481903251827651 //!< log10(e)
+#define A_LN2      0.693147180559945309417 //!< ln(2)
+#define A_LN10     2.30258509299404568402  //!< ln(10)
+#define A_PI       3.14159265358979323846  //!< pi
+#define A_PI_2     1.57079632679489661923  //!< pi/2
+#define A_PI_4     0.785398163397448309616 //!< pi/4
+#define A_1_PI     0.318309886183790671538 //!< 1/pi
+#define A_2_PI     0.636619772367581343076 //!< 2/pi
+#define A_2_SQRTPI 1.12837916709551257390  //!< 2/sqrt(pi)
+#define A_SQRT2    1.41421356237309504880  //!< sqrt(2)
+#define A_SQRT1_2  0.707106781186547524401 //!< 1/sqrt(2)
 
 #define A_SQ(x) ((x) * (x))
 
