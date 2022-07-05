3df283733e99dbcda7efebf6c286aebee6adb323
tqfx <tqfx@foxmail.com>
2022-07-05 14:49:37 +0800

    refactor: containerof -> container_of

--- include/a/def.h
+++ include/a/def.h
@@ -130,9 +130,9 @@
  @param type structure type
  @param member member variable
 */
-#ifndef containerof
-#define containerof(ptr, type, member) ((type *)((size_t)(ptr)-offsetof(type, member)))
-#endif /* containerof */
+#ifndef container_of
+#define container_of(ptr, type, member) ((type *)((size_t)(ptr)-offsetof(type, member)))
+#endif /* container_of */
 
 /*!
  @brief enumeration of return values
