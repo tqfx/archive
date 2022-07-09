135d1037f5e9ab5d20722f7c63247b2b483729cc
tqfx <tqfx@foxmail.com>
2022-07-09 08:47:43 +0800

    fix(math.h): P_ -> M_

--- src/math.h
+++ src/math.h
@@ -9,32 +9,32 @@
 
 #include "a/math.h"
 
-#undef P_E
-#undef P_LOG2E
-#undef P_LOG10E
-#undef P_LN2
-#undef P_LN10
-#undef P_PI
-#undef P_PI_2
-#undef P_PI_4
-#undef P_1_PI
-#undef P_2_PI
-#undef P_2_SQRTPI
-#undef P_SQRT2
-#undef P_SQRT1_2
-#define P_E A_E
-#define P_LOG2E A_LOG2E
-#define P_LOG10E A_LOG10E
-#define P_LN2 A_LN2
-#define P_LN10 A_LN10
-#define P_PI A_PI
-#define P_PI_2 A_PI_2
-#define P_PI_4 A_PI_4
-#define P_1_PI A_1_PI
-#define P_2_PI A_2_PI
-#define P_2_SQRTPI A_2_SQRTPI
-#define P_SQRT2 A_SQRT2
-#define P_SQRT1_2 A_SQRT1_2
+#undef M_E
+#undef M_LOG2E
+#undef M_LOG10E
+#undef M_LN2
+#undef M_LN10
+#undef M_PI
+#undef M_PI_2
+#undef M_PI_4
+#undef M_1_PI
+#undef M_2_PI
+#undef M_2_SQRTPI
+#undef M_SQRT2
+#undef M_SQRT1_2
+#define M_E A_E
+#define M_LOG2E A_LOG2E
+#define M_LOG10E A_LOG10E
+#define M_LN2 A_LN2
+#define M_LN10 A_LN10
+#define M_PI A_PI
+#define M_PI_2 A_PI_2
+#define M_PI_4 A_PI_4
+#define M_1_PI A_1_PI
+#define M_2_PI A_2_PI
+#define M_2_SQRTPI A_2_SQRTPI
+#define M_SQRT2 A_SQRT2
+#define M_SQRT1_2 A_SQRT1_2
 
 #undef DBL_C
 #undef FLT_C
