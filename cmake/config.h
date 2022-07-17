0fddfad98549f0a861905aa8bca4d1f38364b02e
tqfx <tqfx@foxmail.com>
2022-07-17 18:23:59 +0800

    feat(config): A_VERSION_*

--- cmake/config.h
+++ cmake/config.h
@@ -9,6 +9,28 @@
 
 #include "a/def.h"
 
+// clang-format off
+
+#cmakedefine A_VERSION_MAJOR @A_VERSION_MAJOR@
+#cmakedefine A_VERSION_MINOR @A_VERSION_MINOR@
+#cmakedefine A_VERSION_PATCH @A_VERSION_PATCH@
+#cmakedefine A_VERSION_TWEAK @A_VERSION_TWEAK@
+
+// clang-format on
+
+#ifndef A_VERSION_MAJOR
+#define A_VERSION_MAJOR 0
+#endif /* A_VERSION_MAJOR */
+#ifndef A_VERSION_MINOR
+#define A_VERSION_MINOR 0
+#endif /* A_VERSION_MINOR */
+#ifndef A_VERSION_PATCH
+#define A_VERSION_PATCH 0
+#endif /* A_VERSION_PATCH */
+#ifndef A_VERSION_TWEAK
+#define A_VERSION_TWEAK 0
+#endif /* A_VERSION_TWEAK */
+
 /*! algorithm library version string */
 #cmakedefine A_VERSION "@A_VERSION@"
 
