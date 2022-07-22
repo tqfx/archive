6bf91918238b3a27ebcd7be21943cfdc9f6d2dd2
tqfx <tqfx@foxmail.com>
2022-07-23 05:57:59 +0800

    feat: build using xmake

---
+++ xmake/config.h
@@ -0,0 +1,24 @@
+/*!
+ @file config.h
+ @brief algorithm library internal configuration
+ @copyright Copyright (C) 2020 tqfx, All rights reserved.
+*/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "a/def.h"
+
+// clang-format off
+
+#define A_VERSION_MAJOR ${VERSION_MAJOR}
+#define A_VERSION_MINOR ${VERSION_MINOR}
+#define A_VERSION_PATCH ${VERSION_ALTER}
+#define A_VERSION_TWEAK ${VERSION_BUILD}
+
+// clang-format on
+
+/*! algorithm library version string */
+#define A_VERSION "${VERSION}"
+
+#endif /* __CONFIG_H__ */
