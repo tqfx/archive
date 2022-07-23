5f22e64bd010538211d7a8be627868307b8b51ca
tqfx <tqfx@foxmail.com>
2022-07-23 23:00:20 +0800

    fix: build lua java using static library

--- ffi/python/test/test.py
+++ ffi/python/test/test.py
@@ -5,9 +5,7 @@ import os
 sys.path.insert(0, os.getcwd())
 
 import a
-
-print("version", a.version())
-
 import aa
 
-print("version", aa.version())
+if a.version() != aa.version():
+    print("version", a.version(), aa.version())
