6ab278e039ce89588e666127d85b85da4bc6cd57
tqfx <tqfx@foxmail.com>
2022-07-25 03:12:23 +0800

    build(xmake): install header files

--- ffi/lua/xmake.lua
+++ ffi/lua/xmake.lua
@@ -1,21 +1,23 @@
+-- option: with-lua
 option("with-lua")
+    set_default(false)
     set_showmenu(true)
     set_values(false, true)
+    set_description("Enable or disable lua")
 option_end()
 
 if has_config("with-lua") then
     add_requires("lua")
-    target("lua-a")
-        set_group("liba")
+    target("a-lua")
+        add_deps("a")
         set_basename("a")
+        add_packages("lua")
         add_rules("swig.c", {moduletype = "lua"})
         add_files("src/**.c", {swigflags = "-no-old-metatable-bindings"})
         add_defines("A_EXPORTS")
-        add_packages("lua")
-        add_deps("a")
         on_load(function (target)
-            target:set("prefixname", "lib")
             target:set("targetdir", path.join(target:targetdir(), "lua"))
+            target:set("prefixname", "lib")
             if target:is_plat("windows", "mingw") then
                 target:set("extension", ".dll")
             end
--- xmake.lua
+++ xmake.lua
@@ -1,76 +1,120 @@
+-- set project name
+set_project("liba")
+
+-- set xmake minimum version
+set_xmakever("2.5.0")
+
+-- set project version
+set_version("0.1.0", {build = "%Y%m%d%H%M"})
+
+-- set language: c11 c++17
+set_languages("c11", "c++17")
+
+-- set warning everything
+set_warnings("everything")
+
+-- disable some compiler errors
+add_cxflags("-Wno-reserved-id-macro", "-Wno-reserved-identifier", "-Wno-used-but-marked-unused", "-Wno-reserved-macro-identifier")
+add_cflags("-Wno-declaration-after-statement")
+add_cxxflags("-Wno-c++98-compat-pedantic")
+
+-- add build modes
 add_rules("mode.debug", "mode.release")
 
-set_project("liba")
-set_license("MPL-2.0")
-set_version("0.1.0", {build = "%Y%m%d%H%M"})
-set_description("An algorithm library based on C/C++ language")
-
-target()
-    set_languages("c11", "c++17")
-    set_warnings("everything")
-    on_load(function (target)
-        local flags = {
-            "-Wno-reserved-id-macro",
-            "-Wno-reserved-identifier",
-            "-Wno-used-but-marked-unused",
-            "-Wno-reserved-macro-identifier",
-        }
-        target:add("cflags", flags, "-Wno-declaration-after-statement")
-        target:add("cxxflags", flags, "-Wno-c++98-compat-pedantic")
-    end)
-target_end()
-
 target("a")
-    set_group("liba")
+
+    -- make as a static/shared library
     set_kind("$(kind)")
-    add_files("src/**.c")
-    add_includedirs("include", {public = true})
+
+    -- set the auto-generated config.h
     set_configdir("$(curdir)/src")
     add_configfiles("xmake/config.h")
+
+    -- add defines
     add_defines("A_CONFIG")
+
     if is_kind("shared") then
+        -- export symbols
         add_defines("A_EXPORTS")
+        -- import symbols
         add_defines("A_SHARED", {interface = true})
     end
+
+    -- add include directories
+    add_includedirs("include", {public = true})
+
+    -- add the header files for installing
+    add_headerfiles("include/(**.h)")
+
+    -- add the common source files
+    add_files("src/**.c")
+
+    -- add the platform options
     if not is_plat("windows", "mingw") then
         add_syslinks("m")
     end
+
 target_end()
 
 target("aa")
-    set_group("liba")
+
+    -- add the dependent target
+    add_deps("a")
+
+    -- make as a static/shared library
     set_kind("$(kind)")
-    add_files("src/**.cpp")
-    add_includedirs("include", {public = true})
+
+    -- set the auto-generated config.hpp
     set_configdir("$(curdir)/src")
     add_configfiles("xmake/config.hpp")
+
+    -- add defines
     add_defines("A_CONFIG")
+
     if is_kind("shared") then
+        -- export symbols
         add_defines("A_EXPORTS")
+        -- import symbols
         add_defines("A_SHARED", {interface = true})
     end
+
+    -- add include directories
+    add_includedirs("include", {public = true})
+
+    -- add the header files for installing
+    add_headerfiles("include/(**.hpp)")
+
+    -- add the common source files
+    add_files("src/**.cpp")
+
+    -- add the platform options
     if not is_plat("windows", "mingw") then
         add_syslinks("m")
     end
+
 target_end()
 
+-- include ffi sources
 includes("ffi/lua")
 
+-- option: with-rust
 option("with-rust")
+    set_default(false)
     set_showmenu(true)
     set_values(false, true)
+    set_description("Enable or disable rust")
 option_end()
 
 if has_config("with-rust") then
     add_requires("cargo::liba", {configs = {cargo_toml = path.join(os.projectdir(), "Cargo.toml")}})
-    target("rust-c")
-        set_group("liba")
+    target("a-rust")
+        add_deps("a")
         set_basename("a")
         set_kind("$(kind)")
         add_files("src/**.rs")
         add_packages("cargo::liba")
-        add_deps("a")
     target_end()
 end
 
+-- include test sources
 includes("test")
