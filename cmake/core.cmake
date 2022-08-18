95b25ee549abcc67a77754d905b1d8aa2bb8b636
tqfx <tqfx@foxmail.com>
2022-08-19 03:21:42 +0800

    build(core.cmake): / -> -

--- cmake/core.cmake
+++ cmake/core.cmake
@@ -67,7 +67,7 @@ get_property(enabled_languages GLOBAL PROPERTY ENABLED_LANGUAGES)
 
 macro(check_flag_cx)
   foreach(v ${ARGN})
-    string(REGEX REPLACE "[^A-Za-z0-9_/+-]+" "-" r ${v})
+    string(REGEX REPLACE "[^A-Za-z0-9_+-]+" "-" r ${v})
 
     if(C IN_LIST enabled_languages)
       check_c_compiler_flag(${v} C${r})
@@ -89,7 +89,7 @@ endmacro()
 
 macro(check_flag_cc)
   foreach(v ${ARGN})
-    string(REGEX REPLACE "[^A-Za-z0-9_/+-]+" "-" r ${v})
+    string(REGEX REPLACE "[^A-Za-z0-9_+-]+" "-" r ${v})
 
     if(C IN_LIST enabled_languages)
       check_c_compiler_flag(${v} C${r})
@@ -103,7 +103,7 @@ endmacro()
 
 macro(check_flag_xx)
   foreach(v ${ARGN})
-    string(REGEX REPLACE "[^A-Za-z0-9_/+-]+" "-" r ${v})
+    string(REGEX REPLACE "[^A-Za-z0-9_+-]+" "-" r ${v})
 
     if(CXX IN_LIST enabled_languages)
       check_cxx_compiler_flag(${v} CXX${r})
