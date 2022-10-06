66dbc284df4da401449a10e3a911def18681fa07
tqfx <tqfx@foxmail.com>
2022-10-06 18:27:31 +0800

    fix(a_inv_sqrt): -> .

--- src/math.c
+++ src/math.c
@@ -12,21 +12,21 @@ a_f32_t a_inv_sqrt(a_f32_t x)
     {
         a_f32_t x;
         a_u32_t u;
-    } u[1] = {{x}};
+    } u = {x};
     if (a_likely(x > 0))
     {
         a_f32_t xh = 0.5F * x;
-        u->u = 0x5F3759DF - (u->u >> 1);
-        u->x *= 1.5F - SQ(u->x) * xh;
-        u->x *= 1.5F - SQ(u->x) * xh;
+        u.u = 0x5F3759DF - (u.u >> 1);
+        u.x *= 1.5F - SQ(u.x) * xh;
+        u.x *= 1.5F - SQ(u.x) * xh;
     }
     else if (x < 0)
     {
-        u->u = 0xFFC00000;
+        u.u = 0xFFC00000;
     }
     else
     {
-        u->u = 0x7F800000;
+        u.u = 0x7F800000;
     }
-    return u->x;
+    return u.x;
 }
