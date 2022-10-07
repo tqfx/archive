bd0db5741c23e4de53a6ac276cb77d4cfecd90e0
tqfx <tqfx@foxmail.com>
2022-10-07 22:45:31 +0800

    fix: package

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -7,23 +7,13 @@ function(package_cpack root)
     ${root}/.gitignore
     ${root}/.gitmodules
     ${root}/.gitattributes
+    ${PROJECT_BINARY_DIR}
   )
-
-  if(EXISTS ${root}/.gitignore)
-    file(STRINGS ${root}/.gitignore IGNORE NEWLINE_CONSUME)
-    string(REPLACE "\n" ";" IGNORE "${IGNORE}")
-  else()
-    set(IGNORE target cmake-build* build* *.egg-info dist)
-  endif()
-
-  foreach(ignore ${IGNORE})
-    file(GLOB ignore ${ignore})
-    list(APPEND SOURCE_IGNORE_FILES ${ignore})
-  endforeach()
-
   include(InstallRequiredSystemLibraries)
   set(CPACK_SOURCE_IGNORE_FILES ${SOURCE_IGNORE_FILES})
   set(CPACK_RESOURCE_FILE_LICENSE ${root}/LICENSE.txt)
   set(CPACK_RESOURCE_FILE_README ${root}/README.md)
+  set(CPACK_SOURCE_GENERATOR ZIP)
+  set(CPACK_GENERATOR ZIP)
   include(CPack)
 endfunction()
