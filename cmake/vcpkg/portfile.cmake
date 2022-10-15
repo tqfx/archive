7136c6b680e2da717bf9e01c9d1ee09596b6d282
tqfx <tqfx@foxmail.com>
2022-10-15 19:23:29 +0800

    add: ENABLE_PKGCONFIG

--- cmake/vcpkg/portfile.cmake
+++ cmake/vcpkg/portfile.cmake
@@ -4,23 +4,33 @@ vcpkg_from_git(OUT_SOURCE_PATH SOURCE_PATH
   HEAD_REF master
 )
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS_RELEASE
   FEATURES ipo ENABLE_IPO
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES pkgconfig ENABLE_PKGCONFIG
+)
+
 vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS_RELEASE ${FEATURE_OPTIONS}
-  OPTIONS -DBUILD_TESTING=OFF
+  OPTIONS -DBUILD_TESTING=OFF ${FEATURE_OPTIONS}
+  OPTIONS_RELEASE ${OPTIONS_RELEASE}
 )
 
 vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
+if(ENABLE_PKGCONFIG)
+  vcpkg_fixup_pkgconfig()
+endif()
+
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
-vcpkg_fixup_pkgconfig()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
