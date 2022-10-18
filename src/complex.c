5d45407e951168c79d44a2c0c63cb4be72e739fb
tqfx <tqfx@foxmail.com>
2022-10-18 12:55:10 +0800

    fix(complex): return (a_complex_s){}

--- src/complex.c
+++ src/complex.c
@@ -7,8 +7,7 @@
 
 a_complex_s a_complex_polar(a_real_t r, a_real_t theta)
 {
-    a_complex_s z = {r * A_REAL_F(cos, theta), r * A_REAL_F(sin, theta)};
-    return z;
+    return (a_complex_s){r * A_REAL_F(cos, theta), r * A_REAL_F(sin, theta)};
 }
 
 a_real_t a_complex_abs2(a_complex_s z)
@@ -25,94 +24,80 @@ a_real_t a_complex_abs(a_complex_s z)
 #endif
 }
 
-a_complex_s a_complex_inv(a_complex_s z)
-{
-    a_real_t inv = 1 / (z.real * z.real + z.imag * z.imag);
-    z.imag *= -inv;
-    z.real *= inv;
-    return z;
-}
-
 a_real_t a_complex_arg(a_complex_s z)
 {
     return A_REAL_F(atan2, z.imag, z.real);
 }
 
+a_complex_s a_complex_inv(a_complex_s z)
+{
+    a_real_t inv = 1 / (z.real * z.real + z.imag * z.imag);
+    return (a_complex_s){z.real * inv, -z.imag * inv};
+}
+
 a_complex_s a_complex_add(a_complex_s x, a_complex_s y)
 {
-    a_complex_s z = {x.real + y.real, x.imag + y.imag};
-    return z;
+    return (a_complex_s){x.real + y.real, x.imag + y.imag};
 }
 
 a_complex_s a_complex_add_real(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real + y, x.imag};
-    return z;
+    return (a_complex_s){x.real + y, x.imag};
 }
 
 a_complex_s a_complex_add_imag(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real, x.imag + y};
-    return z;
+    return (a_complex_s){x.real, x.imag + y};
 }
 
 a_complex_s a_complex_sub(a_complex_s x, a_complex_s y)
 {
-    a_complex_s z = {x.real - y.real, x.imag - y.imag};
-    return z;
+    return (a_complex_s){x.real - y.real, x.imag - y.imag};
 }
 
 a_complex_s a_complex_sub_real(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real - y, x.imag};
-    return z;
+    return (a_complex_s){x.real - y, x.imag};
 }
 
 a_complex_s a_complex_sub_imag(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real, x.imag - y};
-    return z;
+    return (a_complex_s){x.real, x.imag - y};
 }
 
 a_complex_s a_complex_mul(a_complex_s x, a_complex_s y)
 {
-    a_complex_s z = {
+    return (a_complex_s){
         x.real * y.real - x.imag * y.imag,
         x.real * y.imag + x.imag * y.real,
     };
-    return z;
 }
 
 a_complex_s a_complex_mul_real(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real * y, x.imag * y};
-    return z;
+    return (a_complex_s){x.real * y, x.imag * y};
 }
 
 a_complex_s a_complex_mul_imag(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {-x.imag * y, x.real * y};
-    return z;
+    return (a_complex_s){-x.imag * y, x.real * y};
 }
 
 a_complex_s a_complex_div(a_complex_s x, a_complex_s y)
 {
     a_real_t inv = 1 / (y.real * y.real + y.imag * y.imag);
-    a_complex_s z = {
+    return (a_complex_s){
         (x.real * y.real + x.imag * y.imag) * inv,
         (x.imag * y.real - x.real * y.imag) * inv,
     };
-    return z;
 }
 
 a_complex_s a_complex_div_real(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.real / y, x.imag / y};
-    return z;
+    return (a_complex_s){x.real / y, x.imag / y};
 }
 
 a_complex_s a_complex_div_imag(a_complex_s x, a_real_t y)
 {
-    a_complex_s z = {x.imag / y, -x.real / y};
-    return z;
+    return (a_complex_s){x.imag / y, -x.real / y};
 }
