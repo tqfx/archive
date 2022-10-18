c79e229e192a71df4e32bd5af3b6380934b53ad2
tqfx <tqfx@foxmail.com>
2022-10-18 23:50:54 +0800

    del(math): @brief

--- include/a/math.h
+++ include/a/math.h
@@ -15,31 +15,31 @@
  @{
 */
 
-/*! @brief \f$ e \f$ */
+/*! \f$ e \f$ */
 #define A_E 2.71828182845904523536
-/*! @brief \f$ \log_{2}{e}=\frac{1}{\ln{2}} \f$ */
+/*! \f$ \log_{2}{e}=\frac{1}{\ln{2}} \f$ */
 #define A_LOG2E 1.44269504088896340736
-/*! @brief \f$ \lg{e}=\frac{1}{\ln{2}+\ln{5}} \f$ */
+/*! \f$ \lg{e}=\frac{1}{\ln{2}+\ln{5}} \f$ */
 #define A_LOG10E 0.434294481903251827651
-/*! @brief \f$ \ln{2} \f$ */
+/*! \f$ \ln{2} \f$ */
 #define A_LN2 0.693147180559945309417
-/*! @brief \f$ \ln{10} \f$ */
+/*! \f$ \ln{10} \f$ */
 #define A_LN10 2.30258509299404568402
-/*! @brief \f$ \pi \f$ */
+/*! \f$ \pi \f$ */
 #define A_PI 3.14159265358979323846
-/*! @brief \f$ \frac{\pi}{2} \f$ */
+/*! \f$ \frac{\pi}{2} \f$ */
 #define A_PI_2 1.57079632679489661923
-/*! @brief \f$ \frac{\pi}{4} \f$ */
+/*! \f$ \frac{\pi}{4} \f$ */
 #define A_PI_4 0.785398163397448309616
-/*! @brief \f$ \frac{1}{\pi} \f$ */
+/*! \f$ \frac{1}{\pi} \f$ */
 #define A_1_PI 0.318309886183790671538
-/*! @brief \f$ \frac{2}{\pi} \f$ */
+/*! \f$ \frac{2}{\pi} \f$ */
 #define A_2_PI 0.636619772367581343076
-/*! @brief \f$ \frac{2}{\sqrt{\pi}} \f$ */
+/*! \f$ \frac{2}{\sqrt{\pi}} \f$ */
 #define A_2_SQRTPI 1.12837916709551257390
-/*! @brief \f$ \sqrt{2} \f$ */
+/*! \f$ \sqrt{2} \f$ */
 #define A_SQRT2 1.41421356237309504880
-/*! @brief \f$ \frac{1}{\sqrt{2}}=\frac{\sqrt{2}}{2} \f$ */
+/*! \f$ \frac{1}{\sqrt{2}}=\frac{\sqrt{2}}{2} \f$ */
 #define A_SQRT1_2 0.707106781186547524401
 
 /*!
@@ -97,7 +97,7 @@ extern "C" {
  @brief fast inverse square-root, \f$ \frac{1}{\sqrt{x}} \f$
  @details http://en.wikipedia.org/wiki/Fast_inverse_square_root
  @param[in] x independent variable
- @return result after calculation
+ @return calculated result
 */
 A_PUBLIC a_f32_t a_sqrt_inv(a_f32_t x);
 
