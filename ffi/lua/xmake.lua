dd29faa90ea473ba3295b5d48c7a2b947bfa2ff3
tqfx <tqfx@foxmail.com>
2023-02-02 14:15:40 +0800

    fix: xmake find_package lua

--- ffi/lua/xmake.lua
+++ ffi/lua/xmake.lua
@@ -1,26 +1,28 @@
 -- option: with-lua
 option("with-lua")
-    set_default(false)
     set_showmenu(true)
+    set_values("lua", "luajit")
     set_description("Enable/Disable lua")
 option_end()
 
 if has_config("with-lua") then
-    add_requires("lua")
+    add_requires(get_config("with-lua"))
     target("a.lua")
         set_basename("a")
         set_kind("shared")
         add_deps("a.objs")
-        add_packages("lua")
         add_files("src/**.c")
         set_prefixname("lib")
+        add_packages(get_config("with-lua"))
         if is_plat("windows", "mingw") then
             set_extension(".dll")
         end
         add_defines("A_EXPORTS")
         on_load(function (target)
-            target:set("targetdir", path.join(target:targetdir(), "lua"))
-            target:set("installfiles", target:targetfile(), {prefixdir = "lib/lua/5.4"})
+            local subdir = get_config("with-lua")
+            local libdir = path.join("lib", subdir)
+            target:set("targetdir", path.join(target:targetdir(), subdir))
+            target:set("installfiles", target:targetfile(), {prefixdir = libdir})
         end)
     target_end()
 end
